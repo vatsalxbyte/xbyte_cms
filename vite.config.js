50675f5157ee5bc29648ef23c0bc5647a06f0f0c
vatsalxbyte <vatsal.zinzuvadiya@xbyte.io>
2023-10-31 15:16:31 +0530

    port added in config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000, // Specify the port you want to use
-  },
+  host: 'http://192.168.1.75:3000/',
   plugins: [react()],
 })
